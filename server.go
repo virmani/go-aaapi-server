933e8beac75d7da24a6da297f1597d61a2d8123d
Ashish Virmani <virmani.ashish@gmail.com>
2018-06-11 21:23:57 -0700

    first checkin. added crc check

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"log"
+	"net/http"
+	"os"
+)
+
+const crcTokenParam = "crc_token"
+const webhookPath = "webhooks"
+
+var twitterConsumerKey = os.Getenv("TWITTER_CONSUMER_KEY")
+var twitterConsumerSecret = os.Getenv("TWITTER_CONSUMER_SECRET")
+
+func crcCheck(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	crcToken := params.Get(crcTokenParam)
+
+	if crcToken == "" {
+		log.Printf("No %v found in the query params", crcTokenParam)
+		http.Error(w, "crc_token is not specified in the request query params",
+			http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("crc token found: %v", crcToken)
+
+	mac := hmac.New(sha256.New, []byte(twitterConsumerSecret))
+	mac.Write([]byte(crcToken))
+
+	w.Write([]byte(base64.StdEncoding.EncodeToString(mac.Sum(nil))))
+}
+
+func main() {
+	http.HandleFunc("/"+webhookPath, crcCheck)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
